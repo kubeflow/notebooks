2a1324873122cc325b9f1f82f7c3d55382b84e13
mohamedch7 <121046693+mohamedch7@users.noreply.github.com>
2024-12-18 22:14:32 +0100

    fix(ws): fix workspaceKind type to POD_TEMPLATE
    Signed-off-by: mohamedch7 <121046693+mohamedch7@users.noreply.github.com>

--- workspaces/backend/internal/models/workspaces.go
+++ workspaces/backend/internal/models/workspaces.go
@@ -15,157 +15,156 @@ limitations under the License.
 */
 package models
 
- import (
-	 kubefloworgv1beta1 "github.com/kubeflow/notebooks/workspaces/controller/api/v1beta1"
- )
- 
- type WorkspaceModel struct {
-	 Name          string        `json:"name"`
-	 Namespace     string        `json:"namespace"`
-	 WorkspaceKind WorkspaceKind `json:"workspace_kind"`
-	 DeferUpdates  bool          `json:"defer_updates"`
-	 Paused        bool          `json:"paused"`
-	 PausedTime    int64         `json:"paused_time"`
-	 State         string        `json:"state"`
-	 StateMessage  string        `json:"state_message"`
-	 PodTemplate   PodTemplate   `json:"pod_template"`
-	 Activity      Activity      `json:"activity"`
- }
- type PodTemplate struct {
-	 PodMetadata *PodMetadata `json:"pod_metadata"`
-	 Volumes     *Volumes     `json:"volumes"`
-	 ImageConfig *ImageConfig `json:"image_config"`
-	 PodConfig   *PodConfig   `json:"pod_config"`
- }
- 
- type PodMetadata struct {
-	 Labels      map[string]string `json:"labels"`
-	 Annotations map[string]string `json:"annotations"`
- }
- type Volumes struct {
-	 Home *DataVolumeModel  `json:"home"`
-	 Data []DataVolumeModel `json:"data"`
- }
- 
- type ImageConfig struct {
-	 Current       string           `json:"current"`
-	 Desired       string           `json:"desired"`
-	 RedirectChain []*RedirectChain `json:"redirect_chain"`
- }
- 
- type PodConfig struct {
-	 Current       string           `json:"current"`
-	 Desired       string           `json:"desired"`
-	 RedirectChain []*RedirectChain `json:"redirect_chain"`
- }
- 
- type RedirectChain struct {
-	 Source string `json:"source"`
-	 Target string `json:"target"`
- }
- 
- type Activity struct {
-	 LastActivity int64  `json:"last_activity"` // Unix Epoch time
-	 LastUpdate   int64  `json:"last_update"`   // Unix Epoch time
-	 LastProbe    *Probe `json:"last_probe"`
- }
- 
- type Probe struct {
-	 StartTimeMs int64  `json:"start_time_ms"` // Unix Epoch time in milliseconds
-	 EndTimeMs   int64  `json:"end_time_ms"`   // Unix Epoch time in milliseconds
-	 Result      string `json:"result"`
-	 Message     string `json:"message"`
- }
- 
- type WorkspaceKind struct {
-	 Name string `json:"name"`
-	 Type string `json:"type"`
- }
- 
- type DataVolumeModel struct {
-	 PvcName   string `json:"pvc_name"`
-	 MountPath string `json:"mount_path"`
-	 ReadOnly  bool   `json:"read_only"`
- }
- 
- func NewWorkspaceModelFromWorkspace(item *kubefloworgv1beta1.Workspace) WorkspaceModel {
-	 // t := time.Unix(item.Status.Activity.LastActivity, 0)
-	 // formattedLastActivity := t.Format("2006-01-02 15:04:05 MST")
- 
-	 dataVolumes := make([]DataVolumeModel, len(item.Spec.PodTemplate.Volumes.Data))
-	 for i, volume := range item.Spec.PodTemplate.Volumes.Data {
-		 dataVolumes[i] = DataVolumeModel{
-			 PvcName:   volume.PVCName,
-			 MountPath: volume.MountPath,
-			 ReadOnly:  *volume.ReadOnly,
-		 }
-	 }
- 
-	 imageConfigRedirectChain := make([]*RedirectChain, len(item.Status.PodTemplateOptions.ImageConfig.RedirectChain))
-	 for i, chain := range item.Status.PodTemplateOptions.ImageConfig.RedirectChain {
-		 imageConfigRedirectChain[i] = &RedirectChain{
-			 Source: chain.Source,
-			 Target: chain.Target,
-		 }
-	 }
- 
-	 podConfigRedirectChain := make([]*RedirectChain, len(item.Status.PodTemplateOptions.PodConfig.RedirectChain))
- 
-	 for i, chain := range item.Status.PodTemplateOptions.PodConfig.RedirectChain {
-		 podConfigRedirectChain[i] = &RedirectChain{
-			 Source: chain.Source,
-			 Target: chain.Target,
-		 }
-	 }
- 
-	 workspaceModel := WorkspaceModel{
-		 Name:      item.ObjectMeta.Name,
-		 Namespace: item.Namespace,
-		 WorkspaceKind: WorkspaceKind{
-			 Name: item.Spec.Kind,
-			 Type: item.Spec.PodTemplate.Options.ImageConfig,
-		 },
-		 DeferUpdates: *item.Spec.DeferUpdates,
-		 Paused:       *item.Spec.Paused,
-		 PausedTime:   item.Status.PauseTime,
-		 State:        string(item.Status.State),
-		 StateMessage: item.Status.StateMessage,
-		 PodTemplate: PodTemplate{
-			 PodMetadata: &PodMetadata{
-				 Labels:      item.Spec.PodTemplate.PodMetadata.Labels,
-				 Annotations: item.Spec.PodTemplate.PodMetadata.Annotations,
-			 },
-			 Volumes: &Volumes{
-				 Home: &DataVolumeModel{
-					 PvcName:   item.Spec.PodTemplate.Volumes.Data[0].PVCName,
-					 MountPath: item.Spec.PodTemplate.Volumes.Data[0].MountPath,
-					 ReadOnly:  *item.Spec.PodTemplate.Volumes.Data[0].ReadOnly,
-				 },
-				 Data: dataVolumes,
-			 },
-			 ImageConfig: &ImageConfig{
-				 Current:       item.Spec.PodTemplate.Options.ImageConfig,
-				 Desired:       item.Spec.PodTemplate.Options.ImageConfig,
-				 RedirectChain: imageConfigRedirectChain,
-			 },
-			 PodConfig: &PodConfig{
-				 Current:       item.Spec.PodTemplate.Options.PodConfig,
-				 Desired:       item.Spec.PodTemplate.Options.PodConfig,
-				 RedirectChain: podConfigRedirectChain,
-			 },
-		 },
-		 Activity: Activity{
-			 LastActivity: item.Status.Activity.LastActivity,
-			 LastUpdate:   item.Status.Activity.LastUpdate,
-			 // TODO: update these fields when the last probe is implemented
-			 LastProbe: &Probe{
-				 StartTimeMs: 0,
-				 EndTimeMs:   0,
-				 Result:      "default_result",
-				 Message:     "default_message",
-			 },
-		 },
-	 }
-	 return workspaceModel
- }
- 
+import (
+	kubefloworgv1beta1 "github.com/kubeflow/notebooks/workspaces/controller/api/v1beta1"
+)
+
+type WorkspaceModel struct {
+	Name          string        `json:"name"`
+	Namespace     string        `json:"namespace"`
+	WorkspaceKind WorkspaceKind `json:"workspace_kind"`
+	DeferUpdates  bool          `json:"defer_updates"`
+	Paused        bool          `json:"paused"`
+	PausedTime    int64         `json:"paused_time"`
+	State         string        `json:"state"`
+	StateMessage  string        `json:"state_message"`
+	PodTemplate   PodTemplate   `json:"pod_template"`
+	Activity      Activity      `json:"activity"`
+}
+type PodTemplate struct {
+	PodMetadata *PodMetadata `json:"pod_metadata"`
+	Volumes     *Volumes     `json:"volumes"`
+	ImageConfig *ImageConfig `json:"image_config"`
+	PodConfig   *PodConfig   `json:"pod_config"`
+}
+
+type PodMetadata struct {
+	Labels      map[string]string `json:"labels"`
+	Annotations map[string]string `json:"annotations"`
+}
+type Volumes struct {
+	Home *DataVolumeModel  `json:"home"`
+	Data []DataVolumeModel `json:"data"`
+}
+
+type ImageConfig struct {
+	Current       string           `json:"current"`
+	Desired       string           `json:"desired"`
+	RedirectChain []*RedirectChain `json:"redirect_chain"`
+}
+
+type PodConfig struct {
+	Current       string           `json:"current"`
+	Desired       string           `json:"desired"`
+	RedirectChain []*RedirectChain `json:"redirect_chain"`
+}
+
+type RedirectChain struct {
+	Source string `json:"source"`
+	Target string `json:"target"`
+}
+
+type Activity struct {
+	LastActivity int64  `json:"last_activity"` // Unix Epoch time
+	LastUpdate   int64  `json:"last_update"`   // Unix Epoch time
+	LastProbe    *Probe `json:"last_probe"`
+}
+
+type Probe struct {
+	StartTimeMs int64  `json:"start_time_ms"` // Unix Epoch time in milliseconds
+	EndTimeMs   int64  `json:"end_time_ms"`   // Unix Epoch time in milliseconds
+	Result      string `json:"result"`
+	Message     string `json:"message"`
+}
+
+type WorkspaceKind struct {
+	Name string `json:"name"`
+	Type string `json:"type"`
+}
+
+type DataVolumeModel struct {
+	PvcName   string `json:"pvc_name"`
+	MountPath string `json:"mount_path"`
+	ReadOnly  bool   `json:"read_only"`
+}
+
+func NewWorkspaceModelFromWorkspace(item *kubefloworgv1beta1.Workspace) WorkspaceModel {
+	// t := time.Unix(item.Status.Activity.LastActivity, 0)
+	// formattedLastActivity := t.Format("2006-01-02 15:04:05 MST")
+
+	dataVolumes := make([]DataVolumeModel, len(item.Spec.PodTemplate.Volumes.Data))
+	for i, volume := range item.Spec.PodTemplate.Volumes.Data {
+		dataVolumes[i] = DataVolumeModel{
+			PvcName:   volume.PVCName,
+			MountPath: volume.MountPath,
+			ReadOnly:  *volume.ReadOnly,
+		}
+	}
+
+	imageConfigRedirectChain := make([]*RedirectChain, len(item.Status.PodTemplateOptions.ImageConfig.RedirectChain))
+	for i, chain := range item.Status.PodTemplateOptions.ImageConfig.RedirectChain {
+		imageConfigRedirectChain[i] = &RedirectChain{
+			Source: chain.Source,
+			Target: chain.Target,
+		}
+	}
+
+	podConfigRedirectChain := make([]*RedirectChain, len(item.Status.PodTemplateOptions.PodConfig.RedirectChain))
+
+	for i, chain := range item.Status.PodTemplateOptions.PodConfig.RedirectChain {
+		podConfigRedirectChain[i] = &RedirectChain{
+			Source: chain.Source,
+			Target: chain.Target,
+		}
+	}
+
+	workspaceModel := WorkspaceModel{
+		Name:      item.ObjectMeta.Name,
+		Namespace: item.Namespace,
+		WorkspaceKind: WorkspaceKind{
+			Name: item.Spec.Kind,
+			Type: "POD_TEMPLATE",
+		},
+		DeferUpdates: *item.Spec.DeferUpdates,
+		Paused:       *item.Spec.Paused,
+		PausedTime:   item.Status.PauseTime,
+		State:        string(item.Status.State),
+		StateMessage: item.Status.StateMessage,
+		PodTemplate: PodTemplate{
+			PodMetadata: &PodMetadata{
+				Labels:      item.Spec.PodTemplate.PodMetadata.Labels,
+				Annotations: item.Spec.PodTemplate.PodMetadata.Annotations,
+			},
+			Volumes: &Volumes{
+				Home: &DataVolumeModel{
+					PvcName:   item.Spec.PodTemplate.Volumes.Data[0].PVCName,
+					MountPath: item.Spec.PodTemplate.Volumes.Data[0].MountPath,
+					ReadOnly:  *item.Spec.PodTemplate.Volumes.Data[0].ReadOnly,
+				},
+				Data: dataVolumes,
+			},
+			ImageConfig: &ImageConfig{
+				Current:       item.Spec.PodTemplate.Options.ImageConfig,
+				Desired:       item.Spec.PodTemplate.Options.ImageConfig,
+				RedirectChain: imageConfigRedirectChain,
+			},
+			PodConfig: &PodConfig{
+				Current:       item.Spec.PodTemplate.Options.PodConfig,
+				Desired:       item.Spec.PodTemplate.Options.PodConfig,
+				RedirectChain: podConfigRedirectChain,
+			},
+		},
+		Activity: Activity{
+			LastActivity: item.Status.Activity.LastActivity,
+			LastUpdate:   item.Status.Activity.LastUpdate,
+			// TODO: update these fields when the last probe is implemented
+			LastProbe: &Probe{
+				StartTimeMs: 0,
+				EndTimeMs:   0,
+				Result:      "default_result",
+				Message:     "default_message",
+			},
+		},
+	}
+	return workspaceModel
+}
