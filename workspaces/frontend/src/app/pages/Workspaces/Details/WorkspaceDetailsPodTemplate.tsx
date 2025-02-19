90c2de387febcc85434738b56b06bb2b73449ff8
Liav Weiss (EXT-Nokia) <liav.weiss.ext@nokia.com>
2025-02-19 14:46:03 +0200

    feat(ws): Notebooks 2.0 // Frontend // Workspaces details // Pod template tab kubeflow#173
    Signed-off-by: Liav Weiss (EXT-Nokia) <liav.weiss.ext@nokia.com>

--- workspaces/frontend/src/app/pages/Workspaces/Details/WorkspaceDetailsPodTemplate.tsx
+++ workspaces/frontend/src/app/pages/Workspaces/Details/WorkspaceDetailsPodTemplate.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { CodeEditor, CodeEditorProps, Language } from '@patternfly/react-code-editor';
+import { CodeEditor, Language } from '@patternfly/react-code-editor';
 import yaml from 'js-yaml';
 
 const mockYaml = `apiVersion: kubeflow.org/v1beta1
@@ -23,28 +23,19 @@ spec:
     options:
       imageConfig: "jupyterlab_scipy_190"
       podConfig: "tiny_cpu"`;
-type WorkspaceDetailsPodTemplateProps = Omit<CodeEditorProps, 'ref'> & {
-  testId?: string;
-  codeEditorHeight?: string;
-};
-const parsedYaml = yaml.load(mockYaml);
-const podTemplateYaml = yaml.dump(parsedYaml || {});
-export const WorkspaceDetailsPodTemplate: React.FC<Partial<WorkspaceDetailsPodTemplateProps>> = ({
-  // 38px is the code editor toolbar height+border
-  // calculate the div height to avoid container scrolling
-  height = 'calc(100% - 38px)',
-  codeEditorHeight = '650px',
-  ...props
-}) => (
-  <div data-testid={props.testId} style={{ height, padding: '14px' }}>
+
+export const WorkspaceDetailsPodTemplate: React.FunctionComponent = () => {
+  const parsedYaml = yaml.load(mockYaml);
+  const podTemplateYaml = yaml.dump(parsedYaml || {});
+
+  return (
     <CodeEditor
       isLineNumbersVisible
+      height="100%"
       isReadOnly
       isDownloadEnabled
       code={podTemplateYaml || '# No pod template data available'}
-      height={codeEditorHeight}
       language={Language.yaml}
-      {...props}
     />
-  </div>
-);
+  );
+};
