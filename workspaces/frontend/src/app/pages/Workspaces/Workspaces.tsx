5e32aa1bc18630d1bfcbb8252f03c23de56c0429
Guilherme Caponetto <638737+caponetto@users.noreply.github.com>
2025-06-11 10:24:55 -0300

    feat: Implement usePolling hook and refactor workspace actions in Workspaces and WorkspaceKindSummary components
    Signed-off-by: Guilherme Caponetto <638737+caponetto@users.noreply.github.com>

--- workspaces/frontend/src/app/pages/Workspaces/Workspaces.tsx
+++ workspaces/frontend/src/app/pages/Workspaces/Workspaces.tsx
@@ -1,81 +1,45 @@
 import * as React from 'react';
 import { Content, ContentVariants, PageSection, Stack, StackItem } from '@patternfly/react-core';
-import { IActions } from '@patternfly/react-table';
 import WorkspaceTable from '~/app/components/WorkspaceTable';
 import { useNamespaceContext } from '~/app/context/NamespaceContextProvider';
-import { useWorkspaceActionsContext } from '~/app/context/WorkspaceActionsContext';
 import { useWorkspacesByNamespace } from '~/app/hooks/useWorkspaces';
-import { Workspace, WorkspaceState } from '~/shared/api/backendApiTypes';
+import { WorkspaceState } from '~/shared/api/backendApiTypes';
 import { DEFAULT_POLLING_RATE_MS } from '~/app/const';
 import { LoadingSpinner } from '~/app/components/LoadingSpinner';
 import { LoadError } from '~/app/components/LoadError';
+import { useWorkspaceRowActions } from '~/app/hooks/useWorkspaceRowActions';
+import { usePolling } from '~/app/hooks/usePolling';
 
 export const Workspaces: React.FunctionComponent = () => {
   const { selectedNamespace } = useNamespaceContext();
-  const workspaceActions = useWorkspaceActionsContext();
 
   const [workspaces, workspacesLoaded, workspacesLoadError, refreshWorkspaces] =
     useWorkspacesByNamespace(selectedNamespace);
 
-  React.useEffect(() => {
-    const interval = setInterval(() => {
-      refreshWorkspaces();
-    }, DEFAULT_POLLING_RATE_MS);
-    return () => clearInterval(interval);
-  }, [refreshWorkspaces]);
+  usePolling(refreshWorkspaces, DEFAULT_POLLING_RATE_MS);
 
-  const tableRowActions = React.useCallback(
-    (workspace: Workspace) => {
-      const rowActions: IActions = [
-        {
-          id: 'view-details',
-          title: 'View Details',
-          onClick: () => workspaceActions.requestViewDetailsAction({ workspace }),
-        },
-        // TODO: Uncomment when edit action is fully supported
-        // {
-        //   id: 'edit',
-        //   title: 'Edit',
-        //   onClick: () => workspaceContext.requestEditAction(workspace),
-        // },
-        {
-          id: 'delete',
-          title: 'Delete',
-          onClick: () =>
-            workspaceActions.requestDeleteAction({ workspace, onActionDone: refreshWorkspaces }),
-        },
-        {
-          isSeparator: true,
-        },
-      ];
-
-      if (workspace.state === WorkspaceState.WorkspaceStateRunning) {
-        rowActions.push(
-          {
-            id: 'stop',
-            title: 'Stop',
-            onClick: () =>
-              workspaceActions.requestStopAction({ workspace, onActionDone: refreshWorkspaces }),
-          },
-          {
-            id: 'restart',
-            title: 'Restart',
-            onClick: () =>
-              workspaceActions.requestRestartAction({ workspace, onActionDone: refreshWorkspaces }),
-          },
-        );
-      } else {
-        rowActions.push({
-          id: 'start',
-          title: 'Start',
-          onClick: () =>
-            workspaceActions.requestStartAction({ workspace, onActionDone: refreshWorkspaces }),
-        });
-      }
-      return rowActions;
+  const tableRowActions = useWorkspaceRowActions([
+    { id: 'viewDetails' },
+    // TODO: Uncomment when edit action is fully supported
+    // { id: 'edit' },
+    { id: 'delete', onActionDone: refreshWorkspaces },
+    { id: 'separator' },
+    {
+      id: 'stop',
+      isVisible: (w) => w.state === WorkspaceState.WorkspaceStateRunning,
+      onActionDone: refreshWorkspaces,
     },
-    [refreshWorkspaces, workspaceActions],
-  );
+    {
+      id: 'start',
+      isVisible: (w) => w.state !== WorkspaceState.WorkspaceStateRunning,
+      onActionDone: refreshWorkspaces,
+    },
+    {
+      id: 'restart',
+      isVisible: (w) => w.state === WorkspaceState.WorkspaceStateRunning,
+      onActionDone: refreshWorkspaces,
+    },
+  ]);
 
   if (workspacesLoadError) {
     return <LoadError error={workspacesLoadError} />;
