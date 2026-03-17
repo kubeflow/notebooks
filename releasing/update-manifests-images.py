00c16b32b02ed37922659fcefcecc368bede8db0
Mathew Wicks <5735406+thesuperzapper@users.noreply.github.com>
2026-03-17 15:49:48 -0700

    chore: fix releasing script
    Signed-off-by: Mathew Wicks <5735406+thesuperzapper@users.noreply.github.com>

--- releasing/update-manifests-images.py
+++ releasing/update-manifests-images.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-import argparse
 import logging
+import os
 import sys
 import ruamel.yaml
 
@@ -32,13 +32,10 @@ class YAML(ruamel.yaml.YAML):
 
 yaml = YAML()
 
-applications = [
+components = [
     {
         "name": "Jupyter Web App",
-        "kustomization": (
-            "components/crud-web-apps/jupyter/"
-            "manifests/base/kustomization.yaml"
-        ),
+        "kustomization": "components/crud-web-apps/jupyter/manifests/base/kustomization.yaml",
         "images": [
             {
                 "name": "ghcr.io/kubeflow/notebooks/jupyter-web-app",
@@ -48,80 +45,65 @@ applications = [
     },
     {
         "name": "Tensorboards Web App",
-        "kustomization": (
-            "components/crud-web-apps/tensorboards/"
-            "manifests/base/kustomization.yaml"
-        ),
+        "kustomization": "components/crud-web-apps/tensorboards/manifests/base/kustomization.yaml",
         "images": [
             {
-                "name": "ghcr.io/kubeflow/kubeflow/tensorboards-web-app",
-                "newName": "ghcr.io/kubeflow/kubeflow/tensorboards-web-app",
+                "name": "ghcr.io/kubeflow/notebooks/tensorboards-web-app",
+                "newName": "ghcr.io/kubeflow/notebooks/tensorboards-web-app",
             },
         ],
     },
     {
         "name": "Volumes Web App",
-        "kustomization": (
-            "components/crud-web-apps/volumes/"
-            "manifests/base/kustomization.yaml"
-        ),
+        "kustomization": "components/crud-web-apps/volumes/manifests/base/kustomization.yaml",
         "images": [
             {
-                "name": "ghcr.io/kubeflow/kubeflow/volumes-web-app",
-                "newName": "ghcr.io/kubeflow/kubeflow/volumes-web-app",
+                "name": "ghcr.io/kubeflow/notebooks/volumes-web-app",
+                "newName": "ghcr.io/kubeflow/notebooks/volumes-web-app",
             },
         ],
     },
     {
         "name": "Notebook Controller",
-        "kustomization": (
-            "components/notebook-controller/"
-            "config/base/kustomization.yaml"
-        ),
+        "kustomization": "components/notebook-controller/config/base/kustomization.yaml",
         "images": [
             {
-                "name": "ghcr.io/kubeflow/kubeflow/notebook-controller",
-                "newName": "ghcr.io/kubeflow/kubeflow/notebook-controller",
+                "name": "ghcr.io/kubeflow/notebooks/notebook-controller",
+                "newName": "ghcr.io/kubeflow/notebooks/notebook-controller",
             },
         ],
     },
     {
-        "name": "PVC Viewer Controller",
-        "kustomization": (
-            "components/pvcviewer-controller/"
-            "config/base/kustomization.yaml"
-        ),
+        "name": "PVCViewer Controller",
+        "kustomization": "components/pvcviewer-controller/config/base/kustomization.yaml",
         "images": [
             {
-                "name": "ghcr.io/kubeflow/kubeflow/pvcviewer-controller",
-                "newName": "ghcr.io/kubeflow/kubeflow/pvcviewer-controller",
+                "name": "ghcr.io/kubeflow/notebooks/pvcviewer-controller",
+                "newName": "ghcr.io/kubeflow/notebooks/pvcviewer-controller",
             },
         ],
     },
     {
         "name": "Tensorboard Controller",
-        "kustomization": (
-            "components/tensorboard-controller/"
-            "config/base/kustomization.yaml"
-        ),
+        "kustomization": "components/tensorboard-controller/config/base/kustomization.yaml",
         "images": [
             {
-                "name": "ghcr.io/kubeflow/kubeflow/tensorboard-controller",
-                "newName": "ghcr.io/kubeflow/kubeflow/tensorboard-controller",
+                "name": "ghcr.io/kubeflow/notebooks/tensorboard-controller",
+                "newName": "ghcr.io/kubeflow/notebooks/tensorboard-controller",
             },
         ],
     },
 ]
 
 
-def update_manifests_images(applications, tag):
-    for application in applications:
-        log.info("Updating manifests for app %s", application["name"])
-        with open(application["kustomization"], "r") as file:
+def update_manifests_images(components, tag):
+    for component in components:
+        log.info("Updating manifests for Dashboard component `%s`", component["name"])
+        with open(component["kustomization"], "r") as file:
             kustomize = yaml.load(file)
 
         images = kustomize.get("images", [])
-        for target_image in application["images"]:
+        for target_image in component["images"]:
             found = False
             for image in images:
                 if image["name"] == target_image["name"]:
@@ -130,26 +112,29 @@ def update_manifests_images(applications, tag):
                     found = True
                     break
             if not found:
-                images.append({
-                    "name": target_image["name"],
-                    "newName": target_image["newName"],
-                    "newTag": tag})
+                images.append(
+                    {
+                        "name": target_image["name"],
+                        "newName": target_image["newName"],
+                        "newTag": tag,
+                    }
+                )
         kustomize["images"] = images
 
-        with open(application["kustomization"], "w") as file:
+        with open(component["kustomization"], "w") as file:
             yaml.dump(kustomize, file)
-
-
-def parse_args():
-    parser = argparse.ArgumentParser("Update image tags in manifests.")
-    parser.add_argument("tag", type=str, help="Image tag to use.")
-    return parser.parse_args()
 
 
 def main():
     logging.basicConfig(level=logging.INFO)
-    args = parse_args()
-    update_manifests_images(applications, args.tag)
+
+    # read the tag from the VERSION file
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    version_file_path = os.path.join(base_dir, "./version/VERSION")
+    with open(version_file_path, "r") as file:
+        version = file.read().strip()
+
+    update_manifests_images(components, version)
 
 
 if __name__ == "__main__":
