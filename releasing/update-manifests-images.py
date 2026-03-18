c712293386cb773e963024fb8cab5227148adf14
Mathew Wicks <5735406+thesuperzapper@users.noreply.github.com>
2026-03-18 08:47:38 -0700

    chore: fix releasing script (#969)
    * chore: fix releasing script
    
    Signed-off-by: Mathew Wicks <5735406+thesuperzapper@users.noreply.github.com>
    
    * chore: fix releasing script
    
    Signed-off-by: Mathew Wicks <5735406+thesuperzapper@users.noreply.github.com>
    
    * fix python linting
    
    Signed-off-by: Mathew Wicks <5735406+thesuperzapper@users.noreply.github.com>
    
    * better names for unit test gha
    
    Signed-off-by: Mathew Wicks <5735406+thesuperzapper@users.noreply.github.com>
    
    * pin and update envtest/controller-gen/kustomize
    
    Signed-off-by: Mathew Wicks <5735406+thesuperzapper@users.noreply.github.com>
    
    * use retries for flakey hashicorp go downloads
    
    Signed-off-by: Mathew Wicks <5735406+thesuperzapper@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Mathew Wicks <5735406+thesuperzapper@users.noreply.github.com>

--- releasing/update-manifests-images.py
+++ releasing/update-manifests-images.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-import argparse
 import logging
+import os
 import sys
 import ruamel.yaml
 
@@ -32,13 +32,10 @@ class YAML(ruamel.yaml.YAML):
 
 yaml = YAML()
 
-applications = [
+COMPONENTS = [
     {
         "name": "Jupyter Web App",
-        "kustomization": (
-            "components/crud-web-apps/jupyter/"
-            "manifests/base/kustomization.yaml"
-        ),
+        "kustomization": "components/crud-web-apps/jupyter/manifests/base/kustomization.yaml",
         "images": [
             {
                 "name": "ghcr.io/kubeflow/notebooks/jupyter-web-app",
@@ -48,80 +45,69 @@ applications = [
     },
     {
         "name": "Tensorboards Web App",
-        "kustomization": (
-            "components/crud-web-apps/tensorboards/"
-            "manifests/base/kustomization.yaml"
-        ),
+        "kustomization": "components/crud-web-apps/tensorboards/manifests/base/kustomization.yaml",
         "images": [
             {
-                "name": "ghcr.io/kubeflow/kubeflow/tensorboards-web-app",
-                "newName": "ghcr.io/kubeflow/kubeflow/tensorboards-web-app",
+                "name": "ghcr.io/kubeflow/notebooks/tensorboards-web-app",
+                "newName": "ghcr.io/kubeflow/notebooks/tensorboards-web-app",
             },
         ],
     },
     {
         "name": "Volumes Web App",
-        "kustomization": (
-            "components/crud-web-apps/volumes/"
-            "manifests/base/kustomization.yaml"
-        ),
+        "kustomization": "components/crud-web-apps/volumes/manifests/base/kustomization.yaml",
         "images": [
             {
-                "name": "ghcr.io/kubeflow/kubeflow/volumes-web-app",
-                "newName": "ghcr.io/kubeflow/kubeflow/volumes-web-app",
+                "name": "ghcr.io/kubeflow/notebooks/volumes-web-app",
+                "newName": "ghcr.io/kubeflow/notebooks/volumes-web-app",
             },
         ],
     },
     {
         "name": "Notebook Controller",
-        "kustomization": (
-            "components/notebook-controller/"
-            "config/base/kustomization.yaml"
-        ),
+        "kustomization": "components/notebook-controller/config/base/kustomization.yaml",
         "images": [
             {
-                "name": "ghcr.io/kubeflow/kubeflow/notebook-controller",
-                "newName": "ghcr.io/kubeflow/kubeflow/notebook-controller",
+                "name": "ghcr.io/kubeflow/notebooks/notebook-controller",
+                "newName": "ghcr.io/kubeflow/notebooks/notebook-controller",
             },
         ],
     },
     {
-        "name": "PVC Viewer Controller",
-        "kustomization": (
-            "components/pvcviewer-controller/"
-            "config/base/kustomization.yaml"
-        ),
+        "name": "PVCViewer Controller",
+        "kustomization": "components/pvcviewer-controller/config/base/kustomization.yaml",
         "images": [
             {
-                "name": "ghcr.io/kubeflow/kubeflow/pvcviewer-controller",
-                "newName": "ghcr.io/kubeflow/kubeflow/pvcviewer-controller",
+                "name": "ghcr.io/kubeflow/notebooks/pvcviewer-controller",
+                "newName": "ghcr.io/kubeflow/notebooks/pvcviewer-controller",
             },
         ],
     },
     {
         "name": "Tensorboard Controller",
-        "kustomization": (
-            "components/tensorboard-controller/"
-            "config/base/kustomization.yaml"
-        ),
+        "kustomization": "components/tensorboard-controller/config/base/kustomization.yaml",
         "images": [
             {
-                "name": "ghcr.io/kubeflow/kubeflow/tensorboard-controller",
-                "newName": "ghcr.io/kubeflow/kubeflow/tensorboard-controller",
+                "name": "ghcr.io/kubeflow/notebooks/tensorboard-controller",
+                "newName": "ghcr.io/kubeflow/notebooks/tensorboard-controller",
             },
         ],
     },
 ]
 
+JWA_SPAWNER_CONFIGS = [
+    "components/crud-web-apps/jupyter/manifests/base/configs/spawner_ui_config.yaml",
+]
 
-def update_manifests_images(applications, tag):
-    for application in applications:
-        log.info("Updating manifests for app %s", application["name"])
-        with open(application["kustomization"], "r") as file:
+
+def update_manifests_images(components, tag):
+    for component in components:
+        log.info("Updating manifests for Dashboard component `%s`", component["name"])
+        with open(component["kustomization"], "r") as file:
             kustomize = yaml.load(file)
 
         images = kustomize.get("images", [])
-        for target_image in application["images"]:
+        for target_image in component["images"]:
             found = False
             for image in images:
                 if image["name"] == target_image["name"]:
@@ -130,26 +116,63 @@ def update_manifests_images(applications, tag):
                     found = True
                     break
             if not found:
-                images.append({
-                    "name": target_image["name"],
-                    "newName": target_image["newName"],
-                    "newTag": tag})
+                images.append(
+                    {
+                        "name": target_image["name"],
+                        "newName": target_image["newName"],
+                        "newTag": tag,
+                    }
+                )
         kustomize["images"] = images
 
-        with open(application["kustomization"], "w") as file:
+        with open(component["kustomization"], "w") as file:
             yaml.dump(kustomize, file)
 
 
-def parse_args():
-    parser = argparse.ArgumentParser("Update image tags in manifests.")
-    parser.add_argument("tag", type=str, help="Image tag to use.")
-    return parser.parse_args()
+def update_jwa_spawner_config(config_path, tag):
+    log.info("Updating JWA Spawner config `%s`", config_path)
+    with open(config_path, "r") as file:
+        jwa_config = yaml.load(file)
+
+    # loop over each image type
+    for nb_type in ["image", "imageGroupTwo", "imageGroupOne"]:
+        image_config = jwa_config["spawnerFormDefaults"][nb_type]
+
+        # change the default image
+        old_image = image_config["value"]
+        image_config["value"] = _replace_image_tag(old_image, tag)
+
+        # change the image in each option
+        for i in range(len(image_config["options"])):
+            old_option_image = image_config["options"][i]
+            image_config["options"][i] = _replace_image_tag(old_option_image, tag)
+
+    with open(config_path, "w") as file:
+        yaml.dump(jwa_config, file)
+
+
+def _replace_image_tag(image_string, new_tag):
+    parts = image_string.split(":")
+    if len(parts) != 2:
+        raise RuntimeError("Image `%s` doesn't have expected format <img>:<tag>")
+    return parts[0] + ":" + new_tag
 
 
 def main():
     logging.basicConfig(level=logging.INFO)
-    args = parse_args()
-    update_manifests_images(applications, args.tag)
+
+    # read the tag from the VERSION file
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    version_file_path = os.path.join(base_dir, "./version/VERSION")
+    with open(version_file_path, "r") as file:
+        version = file.read().strip()
+
+    # Update Kustomize manifests with new image tags
+    update_manifests_images(COMPONENTS, version)
+
+    # Update JWA Spawner Configs with new image tags
+    for config_path in JWA_SPAWNER_CONFIGS:
+        update_jwa_spawner_config(config_path, version)
 
 
 if __name__ == "__main__":
