0017ad9af2b9052280b5acd0fbc7c259ac9d3e52
Sven Nobis <snobis@ernw.de>
2026-07-15 02:26:39 +0200

    This new feature makes it possible to host notebooks on their own subdomains when Istio is used. This isolates the notebook's origin from the dashboard / Kubeflow API origin in the browser and addresses a security problem that allows session hijacking through a malicious notebook.
    Signed-off-by: Lorin Lehawany <llehawany@ernw.de>
    Signed-off-by: Sven Nobis <snobis@ernw.de>

--- components/notebook-controller/controllers/notebook_controller_test.go
+++ components/notebook-controller/controllers/notebook_controller_test.go
@@ -1,21 +1,33 @@
 package controllers
 
 import (
+	"bytes"
+	"embed"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"path"
 	"reflect"
 	"testing"
 	"time"
 
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/yaml"
 
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	apiequality "k8s.io/apimachinery/pkg/api/equality"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/client-go/kubernetes/scheme"
 
 	nbv1beta1 "github.com/kubeflow/notebooks/components/notebook-controller/api/v1beta1"
 	ctrl "sigs.k8s.io/controller-runtime"
+
+	_ "embed"
 )
 
 func TestNbNameFromInvolvedObject(t *testing.T) {
@@ -289,10 +301,290 @@ func TestCreateNotebookStatus(t *testing.T) {
 
 }
 
+func TestGenerateVirtualServices(t *testing.T) {
+
+	tests := []struct {
+		name                    string
+		notebook                nbv1beta1.Notebook
+		expectedVirtualServices []*unstructured.Unstructured
+		expectedErrorState      error
+		testEnv                 map[string]string
+	}{
+		{
+			name: "Default config",
+			notebook: nbv1beta1.Notebook{
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "test",
+					Namespace: "kubeflow-user",
+				},
+				Status: nbv1beta1.NotebookStatus{},
+			},
+			expectedVirtualServices: decodeUnstructuredFixture(t, "notebook_controller_virtualservice_test_default_config.yaml"),
+			testEnv:                 map[string]string{},
+		},
+		{
+			name: "Istio host set",
+			notebook: nbv1beta1.Notebook{
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "test",
+					Namespace: "kubeflow-user",
+				},
+				Status: nbv1beta1.NotebookStatus{},
+			},
+			expectedVirtualServices: decodeUnstructuredFixture(t, "notebook_controller_virtualservice_test_istio_host_set.yaml"),
+			testEnv: map[string]string{
+				"ISTIO_HOST": "kubeflow.example.org",
+			},
+		},
+		{
+			name: "Istio gateway set",
+			notebook: nbv1beta1.Notebook{
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "test",
+					Namespace: "kubeflow-user",
+				},
+				Status: nbv1beta1.NotebookStatus{},
+			},
+			expectedVirtualServices: decodeUnstructuredFixture(t, "notebook_controller_virtualservice_test_istio_gateway_set.yaml"),
+			testEnv: map[string]string{
+				"ISTIO_GATEWAY": "test/test-gateway",
+			},
+		},
+		{
+			name: "Cluster domain set",
+			notebook: nbv1beta1.Notebook{
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "test",
+					Namespace: "kubeflow-user",
+				},
+				Status: nbv1beta1.NotebookStatus{},
+			},
+			expectedVirtualServices: decodeUnstructuredFixture(t, "notebook_controller_virtualservice_test_cluster_domain_set.yaml"),
+			testEnv: map[string]string{
+				"CLUSTER_DOMAIN": "example.local",
+			},
+		},
+		{
+			name: "Notebooks subdomain configured",
+			notebook: nbv1beta1.Notebook{
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "test",
+					Namespace: "kubeflow-user",
+				},
+				Status: nbv1beta1.NotebookStatus{},
+			},
+			expectedVirtualServices: decodeUnstructuredFixture(t, "notebook_controller_virtualservice_test_subdomains.yaml"),
+			testEnv: map[string]string{
+				"ISTIO_USE_NOTEBOOK_SUBDOMAINS": "true",
+				"ISTIO_HOST_NOTEBOOK":           "${NAMESPACE}-notebook.kubeflow.example.org",
+				"ISTIO_HOST_AUTH":               "kubeflow.example.org",
+			},
+		},
+		{
+			name: "Notebooks subdomain configured with different auth path",
+			notebook: nbv1beta1.Notebook{
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "test",
+					Namespace: "kubeflow-user",
+				},
+				Status: nbv1beta1.NotebookStatus{},
+			},
+			expectedVirtualServices: decodeUnstructuredFixture(t, "notebook_controller_virtualservice_test_subdomains_different_auth_path.yaml"),
+			testEnv: map[string]string{
+				"ISTIO_USE_NOTEBOOK_SUBDOMAINS": "true",
+				"ISTIO_HOST_NOTEBOOK":           "${NAMESPACE}-notebook.kubeflow.example.org",
+				"ISTIO_HOST_AUTH":               "kubeflow.example.org",
+				"ISTIO_AUTH_PATH":               "/different-auth/",
+			},
+		},
+		{
+			name: "Notebooks invalid subdomain configuration: missing ISTIO_HOST_AUTH",
+			notebook: nbv1beta1.Notebook{
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "test",
+					Namespace: "kubeflow-user",
+				},
+				Status: nbv1beta1.NotebookStatus{},
+			},
+			expectedErrorState: fmt.Errorf("generate virtual service for auth redirect error: invalid ISTIO_HOST_AUTH: When ISTIO_USE_NOTEBOOK_SUBDOMAINS is set, the ISTIO_HOST_AUTH environment variable must be defined."),
+			testEnv: map[string]string{
+				"ISTIO_USE_NOTEBOOK_SUBDOMAINS": "true",
+				"ISTIO_HOST_NOTEBOOK":           "${NAMESPACE}-notebook.kubeflow.example.org",
+			},
+		},
+		{
+			name: "Notebooks invalid subdomain configuration: missing ISTIO_HOST_NOTEBOOK",
+			notebook: nbv1beta1.Notebook{
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "test",
+					Namespace: "kubeflow-user",
+				},
+				Status: nbv1beta1.NotebookStatus{},
+			},
+			expectedErrorState: fmt.Errorf("invalid ISTIO_HOST_NOTEBOOK: When ISTIO_USE_NOTEBOOK_SUBDOMAINS is set, the placeholder ${NAMESPACE} must be defined in ISTIO_HOST_NOTEBOOK."),
+			testEnv: map[string]string{
+				"ISTIO_USE_NOTEBOOK_SUBDOMAINS": "true",
+				"ISTIO_HOST_AUTH":               "kubeflow.example.org",
+			},
+		},
+		{
+			name: "Notebooks invalid subdomain configuration: invalid ISTIO_HOST_NOTEBOOK",
+			notebook: nbv1beta1.Notebook{
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "test",
+					Namespace: "kubeflow-user",
+				},
+				Status: nbv1beta1.NotebookStatus{},
+			},
+			expectedErrorState: fmt.Errorf("invalid ISTIO_HOST_NOTEBOOK: When ISTIO_USE_NOTEBOOK_SUBDOMAINS is set, the placeholder ${NAMESPACE} must be defined in ISTIO_HOST_NOTEBOOK."),
+			testEnv: map[string]string{
+				"ISTIO_USE_NOTEBOOK_SUBDOMAINS": "true",
+				"ISTIO_HOST_NOTEBOOK":           "${NAMESPACE-notebook.kubeflow.example.org",
+				"ISTIO_HOST_AUTH":               "kubeflow.example.org",
+			},
+		},
+		{
+			name: "Notebooks header annotation configured",
+			notebook: nbv1beta1.Notebook{
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "test",
+					Namespace: "kubeflow-user",
+					Annotations: map[string]string{
+						"notebooks.kubeflow.org/http-headers-request-set": `{"ExampleHeader": "Example-Value", "ExampleHeader2": "Example-Value2"}`,
+					},
+				},
+				Status: nbv1beta1.NotebookStatus{},
+			},
+			expectedVirtualServices: decodeUnstructuredFixture(t, "notebook_controller_virtualservice_test_headers.yaml"),
+			testEnv:                 map[string]string{},
+		},
+		{
+			name: "Notebooks invalid header annotation configured",
+			notebook: nbv1beta1.Notebook{
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "test",
+					Namespace: "kubeflow-user",
+					Annotations: map[string]string{
+						"notebooks.kubeflow.org/http-headers-request-set": `{"ExampleHeader": "Example-INVALID_JSON}`,
+					},
+				},
+				Status: nbv1beta1.NotebookStatus{},
+			},
+			expectedVirtualServices: decodeUnstructuredFixture(t, "notebook_controller_virtualservice_test_default_config.yaml"),
+			testEnv:                 map[string]string{},
+		},
+		{
+			name: "Notebooks rewrite annotation configured",
+			notebook: nbv1beta1.Notebook{
+				ObjectMeta: v1.ObjectMeta{
+					Name:      "test",
+					Namespace: "kubeflow-user",
+					Annotations: map[string]string{
+						"notebooks.kubeflow.org/http-rewrite-uri": `/foo/bar`,
+					},
+				},
+				Status: nbv1beta1.NotebookStatus{},
+			},
+			expectedVirtualServices: decodeUnstructuredFixture(t, "notebook_controller_virtualservice_test_rewrite.yaml"),
+			testEnv:                 map[string]string{},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			prepareTestEnvironment(t, test.testEnv)
+
+			actualVirtualServices, actualErrorState := generateVirtualServices(&test.notebook)
+
+			if test.expectedErrorState != nil {
+				if test.expectedErrorState.Error() != actualErrorState.Error() {
+					t.Errorf("Unexpected error: %v\nExpected: %v", actualErrorState, test.expectedErrorState.Error())
+				}
+			} else {
+				if actualErrorState != nil {
+					t.Errorf("Unexpected error: %v", actualErrorState)
+				}
+
+				if len(actualVirtualServices) != len(test.expectedVirtualServices) {
+					t.Errorf("\nExpect: len()=%v; \nOutput: len()=%v", len(test.expectedVirtualServices), len(actualVirtualServices))
+				}
+
+				for i, expected := range test.expectedVirtualServices {
+					var actual *unstructured.Unstructured
+					if len(actualVirtualServices) > i {
+						actual = actualVirtualServices[i]
+					}
+					if !apiequality.Semantic.DeepEqual(expected, actual) {
+						t.Errorf("\nExpect: %v; \nOutput: %v", expected, actual)
+					}
+				}
+			}
+		})
+	}
+
+}
+
 func createMockReconciler() *NotebookReconciler {
 	reconciler := &NotebookReconciler{
 		Scheme: runtime.NewScheme(),
 		Log:    ctrl.Log,
 	}
 	return reconciler
+}
+
+func prepareTestEnvironment(t *testing.T, testEnv map[string]string) {
+	// save old environment & set test environment
+	oldEnv := make(map[string]string)
+	for key, value := range testEnv {
+		oldValue, hadValue := os.LookupEnv(key)
+		if hadValue {
+			oldEnv[key] = oldValue
+			t.Cleanup(func() {
+				os.Setenv(key, oldValue)
+			})
+		} else {
+			t.Cleanup(func() {
+				os.Unsetenv(key)
+			})
+		}
+		os.Setenv(key, value)
+	}
+
+}
+
+//go:embed test_fixtures/*.yaml
+var fixtures embed.FS
+
+func decodeUnstructuredFixture(t *testing.T, name string) []*unstructured.Unstructured {
+	t.Helper()
+
+	data, err := fixtures.ReadFile(path.Join("test_fixtures", name))
+	if err != nil {
+		t.Fatalf("failed to read fixture: %v", err)
+	}
+
+	var objects []*unstructured.Unstructured
+
+	decoder := yaml.NewYAMLOrJSONDecoder(bytes.NewReader(data), 4096)
+
+	for {
+		var obj unstructured.Unstructured
+
+		err := decoder.Decode(&obj)
+		if errors.Is(err, io.EOF) {
+			break
+		}
+		if err != nil {
+			t.Fatalf("failed to decode YAML fixture: %v", err)
+		}
+
+		// Skip empty YAML documents.
+		if len(obj.Object) == 0 {
+			continue
+		}
+
+		objects = append(objects, &obj)
+	}
+
+	return objects
 }
