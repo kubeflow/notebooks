1e9e466f21bbf5896e638ad868e98d27b6bd630a
Siyuan Zhang <10984162+siyuanfoundation@users.noreply.github.com>
2026-08-19 13:53:41 -0500

    feat: implement activity rules in controller (#1279)
    * feat: implment activity rules in controller
    
    Signed-off-by: siyuanfoundation <sizhang@google.com>
    
    * address review comments
    
    Co-authored-by: Christian Heusel <christian@heusel.eu>
    Co-authored-by: Andy Stoneberg <astonebe@redhat.com>
    Signed-off-by: Siyuan Zhang <10984162+siyuanfoundation@users.noreply.github.com>
    
    * change naming of culling to activity rules.
    
    Signed-off-by: siyuanfoundation <sizhang@google.com>
    
    * Apply review comments
    
    Co-authored-by: Andy Stoneberg <astonebe@redhat.com>
    Signed-off-by: Siyuan Zhang <10984162+siyuanfoundation@users.noreply.github.com>
    
    * make has_activity take precedence over last_activity in ActivityProbePodExec
    
    Signed-off-by: Siyuan Zhang <sizhang@google.com>
    
    ---------
    
    Signed-off-by: siyuanfoundation <sizhang@google.com>
    Signed-off-by: Siyuan Zhang <10984162+siyuanfoundation@users.noreply.github.com>
    Signed-off-by: Siyuan Zhang <sizhang@google.com>
    Co-authored-by: Christian Heusel <christian@heusel.eu>
    Co-authored-by: Andy Stoneberg <astonebe@redhat.com>

--- workspaces/controller/internal/controller/suite_test.go
+++ workspaces/controller/internal/controller/suite_test.go
@@ -55,7 +55,9 @@ var (
 	testEnv *envtest.Environment
 	cfg     *rest.Config
 
-	k8sClient client.Client
+	k8sClient           client.Client
+	k8sManager          ctrl.Manager
+	workspaceReconciler *WorkspaceReconciler
 
 	ctx    context.Context
 	cancel context.CancelFunc
@@ -101,7 +103,7 @@ var _ = BeforeSuite(func() {
 	Expect(k8sClient).NotTo(BeNil())
 
 	By("setting up the controller manager")
-	k8sManager, err := ctrl.NewManager(cfg, ctrl.Options{
+	k8sManager, err = ctrl.NewManager(cfg, ctrl.Options{
 		Scheme: scheme.Scheme,
 		Metrics: metricsserver.Options{
 			BindAddress: "0", // disable metrics serving
@@ -120,11 +122,12 @@ var _ = BeforeSuite(func() {
 	Expect(err).NotTo(HaveOccurred())
 
 	By("setting up the Workspace controller")
-	err = (&WorkspaceReconciler{
+	workspaceReconciler = &WorkspaceReconciler{
 		Client: k8sManager.GetClient(),
 		Scheme: k8sManager.GetScheme(),
 		Config: envConfig,
-	}).SetupWithManager(k8sManager, &controller.Options{
+	}
+	err = workspaceReconciler.SetupWithManager(k8sManager, &controller.Options{
 		RateLimiter: helper.BuildRateLimiter(),
 	})
 	Expect(err).NotTo(HaveOccurred())
