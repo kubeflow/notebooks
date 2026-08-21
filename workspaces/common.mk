e6079dd52603e6bb851d10cdb701cba291a5685d
Hammed Akinwale <hammedakinwale35@gmail.com>
2026-08-21 16:19:45 +0100

    fix: address review comments and fixed make help outputs
    Signed-off-by: Hammed Akinwale <hammedakinwale35@gmail.com>

--- workspaces/common.mk
+++ workspaces/common.mk
@@ -1,10 +1,17 @@
 GIT_COMMIT     := $(shell git rev-parse HEAD)
+
 GIT_TREE_STATE := $(shell test -n "`git status --porcelain`" && echo "-dirty" || echo "")
 
 # Location to install dependencies to
 LOCALBIN ?= $(shell pwd)/bin
 $(LOCALBIN):
 	mkdir -p $(LOCALBIN)
+
+# Shared help display for workspace Makefiles.
+# Prints only populated categories and normalizes section titles.
+.PHONY: help
+help: ## Display this help.
+	@awk 'BEGIN { FS = ":.*##"; printf "\nUsage:\n  make \033[36m<target>\033[0m\n" } /^##@/ { current_section = substr($$0, 5); gsub(/^[[:space:]]+/, "", current_section); gsub(/[[:space:]]+$$/, "", current_section); if (current_section == "") current_section = "General"; current_section = toupper(substr(current_section, 1, 1)) substr(current_section, 2); next } /^[a-zA-Z_0-9-]+:.*?##/ && $$1 != "help" { if (!seen_section[current_section]++) printf "\n\033[1m%s\033[0m\n", current_section; printf "  \033[36m%-18s\033[0m %s\n", $$1, $$2 }' $(MAKEFILE_LIST)
 
 ##@ Clean
 
