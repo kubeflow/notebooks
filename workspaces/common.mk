dd004b4c2ef079889fa8809114927fa302193aeb
Hammed Akinwale <hammedakinwale35@gmail.com>
2026-08-21 18:38:31 +0100

    fix: addressed and removed duplicated make help command
    Signed-off-by: Hammed Akinwale <hammedakinwale35@gmail.com>

--- workspaces/common.mk
+++ workspaces/common.mk
@@ -7,12 +7,6 @@ LOCALBIN ?= $(shell pwd)/bin
 $(LOCALBIN):
 	mkdir -p $(LOCALBIN)
 
-# Shared help display for workspace Makefiles.
-# Prints only populated categories and normalizes section titles.
-.PHONY: help
-help: ## Display this help.
-	@awk 'BEGIN { FS = ":.*##"; printf "\nUsage:\n  make \033[36m<target>\033[0m\n" } /^##@/ { current_section = substr($$0, 5); gsub(/^[[:space:]]+/, "", current_section); gsub(/[[:space:]]+$$/, "", current_section); if (current_section == "") current_section = "General"; current_section = toupper(substr(current_section, 1, 1)) substr(current_section, 2); next } /^[a-zA-Z_0-9-]+:.*?##/ && $$1 != "help" { if (!seen_section[current_section]++) printf "\n\033[1m%s\033[0m\n", current_section; printf "  \033[36m%-18s\033[0m %s\n", $$1, $$2 }' $(MAKEFILE_LIST)
-
 ##@ General
 
 # The help target prints out all targets with their descriptions organized
